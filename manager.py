4821ddf3d508ec79b716e7b361b180a5a9e96c7a
tzirkus <zalmen5779@gmail.com>
2025-07-21 10:11:31 +0300

    fix project

--- manager.py
+++ manager.py
@@ -1,18 +1,13 @@
 from interactor import Interactor
 from training import Training
 from prediction import Checker
-from sklearn.model_selection import train_test_split
-import pandas as pd
+
 
 class Manager:
     def __init__(self):
         self.interact = None
         self.df = None
-        self.df_30 = None
-        self.df_70 = None
-        self.Trained_70_dict = {}
         self.Trained_dict = {}
-        self.predict_df = None
 
     def update_model(self,data_url):
         self.interact = Interactor(data_url)
