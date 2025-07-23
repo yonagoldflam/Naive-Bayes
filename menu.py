7f038e5e03e05b35719c26401c867347b06b6c1d
tzirkus <zalmen5779@gmail.com>
2025-07-23 17:00:33 +0300

    update for docker

--- menu.py
+++ menu.py
@@ -2,11 +2,12 @@ from  request import Request
 class Menu:
     def __init__(self):
         self.request = Request('http://127.0.0.1:8080')
+        self.row_request = Request('http://127.0.0.1:8081')
 
     def menu(self):
         flag = True
         while flag:
-            match input('Choose one of the options below: \n1. Update the model \n2. Validate model \n3. Predict a row\nany key to Exit:\nEnter your choice: \n'):
+            match input('Choose one of the options below: \n1. Update the model \n2. Validate model \n3. Predict a row\nany key to Exit:\n4. select data for prediction \nEnter your choice: \n'):
                 case '1':
                     data_url = input('Enter the data url: ')
                     self.request.update_model(data_url)
@@ -16,6 +17,18 @@ class Menu:
                 case '3':
                     body = eval(input('enter row dict: '))
                     self.request.predict_row_request(body)
+
+                case '4':
+                    data_name = input('Enter the data name: ')
+                    self.request.select_data(data_name)
+
+                case '5':
+                    self.request.get_trained_model()
+
+                case '6':
+                    body = eval(input('enter row dict: '))
+                    self.row_request.classify_row_request(body)
+
                 case _:
                     flag = False
 a = Menu()
@@ -23,3 +36,4 @@ a.menu()
 
 # C:/Users/HOME/OneDrive/שולחן העבודה/data python/buy_computer_data.csv
 # {'age': '<=30', 'income':'medium','student': 'yes','credit_rating':'fair'}
+# C:/Users/HOME/OneDrive/שולחן העבודה/data python/PythonProject3/models/buy_computer_data.csv
--- request.py
+++ request.py
@@ -1,7 +1,14 @@
 import requests
+
+
 class Request:
     def __init__(self, base_url):
         self.base_url = base_url
+
+    def classify_row_request(self, row_dict):
+        response = requests.post(f"{self.base_url}/classify", json=row_dict)
+        print(response.status_code)
+        print(response.text)
 
     def predict_row_request(self, row_dict):
         response = requests.post(f"{self.base_url}/predict", json=row_dict)
@@ -9,7 +16,7 @@ class Request:
         print(response.text)
 
     def update_model(self, data_url):
-        response = requests.get(f"{self.base_url}/update", params={"data_url":data_url})
+        response = requests.get(f"{self.base_url}/update", params={"data_url": data_url})
         print(response.status_code)
         print(response.text)
 
@@ -18,4 +25,20 @@ class Request:
         print(response.status_code)
         print(response.text)
 
+    def get_trained_model(self):
+            response = requests.get(f"{self.base_url}/trained")
+            print(response.status_code)
+            print(response.text)
 
+    def select_data(self,model_name):
+        response = requests.get(f"{self.base_url}/select", params={"model_name": model_name})
+        print(response.status_code)
+        print(response.text)
+
+
+
+
+
+
+
+#   curl -X POST http://127.0.0.1:8081/classify \-H "Content-Type: application/json" \-d "{\"age\": \"<=30\", \"income\": \"medium\", \"student\": \"yes\", \"credit_rating\": \"fair\"}"
